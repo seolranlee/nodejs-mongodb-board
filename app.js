5f613459680b30feecfe90580740d9858b97af2b
seolran <leesugar90@gmail.com>
2019-05-22 11:57:04 +0900

    [nodejs-mongodb-board] - 서버실행 - static (정적) 폴더 세팅 - Dynamic web (다이나믹 웹) 만들기 Route & EJS

--- app.js
+++ app.js
@@ -2,25 +2,28 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-// app.use(express.static(__dirname + '/public'));
-// console.log(__dirname); // __dirname 은 node 에서 제공하는 node 파일의 경로를 담고 있는 변수이다.
-
+app.set("view engine", 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 실행결과도 같은데 왜 코드를 더 입력해서 이렇게 하냐면, path를 쓰면
-//
-// path.join('a', 'b')
-//
-// path.join('a/', '/b')
-//
-// path.join('a', '/b')
-//
-// path.join('a/', 'b') 등등..
-//
-// /에 상관없이 주소조합을 알아서 해줍니다.
+var data={count: 0};
+app.get('/',function (req, res) {
+    data.count++;
+    res.render('my_first_ejs', data);
+});
 
-app.get('/', function (req, res) {
-    res.send('Hello world!');
+app.get('/reset', function (req, res) {
+    data.count = 0;
+    res.render('my_first_ejs', data);
+});
+
+app.get('/set/count', function (req, res) {
+    if(req.query.count) data.count=req.query.count; // http://localhost:3000/set/count?count=221
+    res.render('my_first_ejs', data);
+});
+
+app.get('/set/:num', function (req, res) {
+    data.count = req.params.num;    // http://localhost:3000/set/309
+    res.render('my_first_ejs', data);
 });
 
 app.listen(3000, function () {
