35dc46f59c901ebca74b552afb3fcc39f0661945
seolran <leesugar90@gmail.com>
2019-05-22 11:34:10 +0900

    [nodejs-mongodb-board] - 서버실행 - static (정적) 폴더 세팅

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
+
+// app.use(express.static(__dirname + '/public'));
+// console.log(__dirname); // __dirname 은 node 에서 제공하는 node 파일의 경로를 담고 있는 변수이다.
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 실행결과도 같은데 왜 코드를 더 입력해서 이렇게 하냐면, path를 쓰면
+//
+// path.join('a', 'b')
+//
+// path.join('a/', '/b')
+//
+// path.join('a', '/b')
+//
+// path.join('a/', 'b') 등등..
+//
+// /에 상관없이 주소조합을 알아서 해줍니다.
 
 app.get('/', function (req, res) {
     res.send('Hello world!');
